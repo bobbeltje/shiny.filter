6c9054b4ddb3b7bced5905e2fb6cad773744cc60
Matthias <matthias.wubs@syngenta.com>
2021-08-23 15:28:25 +0200

    filter scaffold

---
+++ R/filter.R
@@ -0,0 +1,52 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+filter <- function(message, width = NULL, height = NULL, elementId = NULL) {
+
+  # forward options using x
+  x = list(
+    message = message
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'filter',
+    x,
+    width = width,
+    height = height,
+    package = 'shiny.filter',
+    elementId = elementId
+  )
+}
+
+#' Shiny bindings for filter
+#'
+#' Output and render functions for using filter within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a filter
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name filter-shiny
+#'
+#' @export
+filterOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'filter', width, height, package = 'shiny.filter')
+}
+
+#' @rdname filter-shiny
+#' @export
+renderFilter <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, filterOutput, env, quoted = TRUE)
+}
